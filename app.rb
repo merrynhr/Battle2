d98de57dbef8a933dcafb066e055003b533af006
Cynthia Fu <yinnycfu@gmail.com>
2021-06-10 14:35:01 +0100

    post redirect get pattern

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
@@ -11,11 +12,18 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    p @p1_name = params[:p1_name]
-    p @p2_name = params[:p2_name]
+    session[:p1_name] = params[:p1_name]
+    session[:p2_name] = params[:p2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @p1_name = session[:p1_name]
+    @p2_name = session[:p1_name]
     erb(:play)
   end
 
+  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
